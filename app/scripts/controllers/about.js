19d1b3a122830e58950dd2cc5241b85e6a28ab54
yanninho <y4nn5m@gmail.com>
2015-11-15 23:46:21 +0100

    init

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name appdefaultApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the appdefaultApp
+ */
+angular.module('appdefaultApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name appdefaultApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the appdefaultApp
+ */
+angular.module('appdefaultApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
